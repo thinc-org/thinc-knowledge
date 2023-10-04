d7200a2d829c8ac2f7653dabf9bd94f3af4a3d86
Ebonian <tanadon.santisan@gmail.com>
2023-10-04 17:21:05 +0700

    chore: init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Thinc Roadmap";
+export const SITE_DESCRIPTION = "Thinc First Act Roadmap";
