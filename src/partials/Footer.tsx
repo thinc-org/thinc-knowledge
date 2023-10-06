f9cb364145099750bf115f07d88e644d0a251283
Ebonian <tanadon.santisan@gmail.com>
2023-10-07 00:06:03 +0700

    chore: footer layout

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,7 +1,17 @@
 export const Footer = () => {
     return (
         <section className="flex w-full justify-center">
-            <div className="w-full max-w-screen-lg px-8">Footer</div>
+            <div className="flex w-full max-w-screen-lg flex-col items-center px-8 pb-20 pt-10">
+                <div className="w-24">
+                    <img
+                        className="h-full w-auto select-none"
+                        src="/images/thinc-logo-alt.svg"
+                        alt="Thinc Logo"
+                        loading="eager"
+                    />
+                </div>
+                <p className="mb-0 mt-4 text-slate-400">© THINC. 2023</p>
+            </div>
         </section>
     );
 };
