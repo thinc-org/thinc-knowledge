d7d14f817816e7a06c8cbd6a5377a86ac5fbd8df
Ebonian <tanadon.santisan@gmail.com>
2023-10-07 01:14:04 +0700

    chore: remove rss

--- src/partials/Landing.tsx
+++ src/partials/Landing.tsx
@@ -2,16 +2,20 @@ import { Section } from '@/components/Section';
 
 export const Hero = () => {
     return (
-        <Section className="h-[65vh] bg-gradient-to-b from-thinc-blue to-slate-950 md:h-[70vh]">
+        <Section className="h-[60vh] bg-gradient-to-b from-thinc-blue to-slate-950 md:h-[65vh]">
             <div className="flex flex-col items-center space-y-6 py-16 text-center md:py-20">
+                <span className="-mb-3.5 font-semibold uppercase text-thinc-accent">
+                    Get Ready For
+                </span>
                 <h1 className="text-3xl font-semibold md:text-5xl">
                     Thinc First Act 2023
                 </h1>
                 <p className="max-w-xl text-slate-400">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Adipisci beatae eveniet vero a consectetur pariatur at
-                    laboriosam perferendis libero dolorum autem non blanditiis
-                    nostrum.
+                    กิจกรรมที่พี่ ๆ ในชมรม Thinc.
+                    จะมาสอนและแชร์วิชาในการพัฒนาโปรแกรมพร้อมทั้งวิธีการ
+                    Implement ผลงานตัวเองในแบบพื้นฐานที่สุด
+                    ในหลากหลายสายงานอย่าง Frontend, Backend, Project Manager,
+                    และ UX/UI
                 </p>
             </div>
         </Section>
@@ -34,16 +38,20 @@ export const Roadmap = ({
         <Section className="min-h-screen bg-white text-slate-600">
             <div className="flex w-full flex-col space-y-16">
                 <div className="space-y-6">
-                    <div className="space-y-1 pb-6">
+                    <div className="space-y-1">
                         <h2 className="text-xl font-bold text-slate-800 md:text-2xl">
                             Developer Roadmap
                         </h2>
                         <p>เริ่มต้นเรียนรู้ก่อนใคร...</p>
                     </div>
                     {roadmaps.map((roadmap) => (
-                        <a key={roadmap.id} href={`/roadmap/${roadmap.slug}`}>
-                            <div className="h-40 w-full cursor-pointer rounded-lg border-2 bg-slate-50"></div>
-                        </a>
+                        <div className="flex h-40 w-full rounded-lg border-2 bg-slate-50">
+                            <a
+                                key={roadmap.id}
+                                href={`/roadmap/${roadmap.slug}`}
+                                className="h-full w-full"
+                            ></a>
+                        </div>
                     ))}
                     <div className="relative h-40 select-none opacity-80">
                         <div className="absolute z-10 grid h-full w-full place-content-center bg-gradient-to-b from-transparent to-white"></div>
--- src/partials/Navigation.tsx
+++ src/partials/Navigation.tsx
@@ -1,13 +1,17 @@
 export const Navigation = () => {
     return (
         <header className="flex w-full justify-center">
-            <div className="h-20 w-full max-w-screen-lg px-8 py-6 md:h-24">
-                <img
-                    className="h-full w-auto select-none"
-                    src="/images/thinc-logo-alt.svg"
-                    alt="Thinc Logo"
-                    loading="eager"
-                />
+            <div className="h-20 w-full max-w-screen-lg px-8 md:h-24">
+                <div className="mt-2 h-20 py-5">
+                    <a href="/">
+                        <img
+                            className="h-full w-auto select-none"
+                            src="/images/thinc-logo-alt.svg"
+                            alt="Thinc Logo"
+                            loading="eager"
+                        />
+                    </a>
+                </div>
             </div>
         </header>
     );
