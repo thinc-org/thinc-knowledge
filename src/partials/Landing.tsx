0d2018b75c51ca518a7f3eeea3ec165bbe404dde
Ebonian <tanadon.santisan@gmail.com>
2023-10-06 23:57:38 +0700

    chore: cleanup

--- src/partials/Landing.tsx
+++ src/partials/Landing.tsx
@@ -18,16 +18,33 @@ export const Hero = () => {
     );
 };
 
-export const Roadmap = () => {
+export const Roadmap = ({
+    roadmaps,
+}: {
+    roadmaps: {
+        id: string;
+        slug: string;
+        data: {
+            title: string;
+            description: string;
+        };
+    }[];
+}) => {
     return (
         <Section className="min-h-screen bg-white text-slate-600">
             <div className="flex w-full flex-col space-y-16">
                 <div className="space-y-6">
-                    <h2 className="text-xl font-bold text-slate-800 md:text-2xl">
-                        Developer Roadmap
-                    </h2>
-                    <div className="h-40 w-full cursor-pointer rounded-lg border-2 bg-slate-50"></div>
-                    <div className="h-40 w-full cursor-pointer rounded-lg border-2 bg-slate-50"></div>
+                    <div className="space-y-1 pb-6">
+                        <h2 className="text-xl font-bold text-slate-800 md:text-2xl">
+                            Developer Roadmap
+                        </h2>
+                        <p>เริ่มต้นเรียนรู้ก่อนใคร...</p>
+                    </div>
+                    {roadmaps.map((roadmap) => (
+                        <a key={roadmap.id} href={`/roadmap/${roadmap.slug}`}>
+                            <div className="h-40 w-full cursor-pointer rounded-lg border-2 bg-slate-50"></div>
+                        </a>
+                    ))}
                     <div className="relative h-40 select-none opacity-80">
                         <div className="absolute z-10 grid h-full w-full place-content-center bg-gradient-to-b from-transparent to-white"></div>
                         <div className="absolute grid h-full w-full place-content-center rounded-lg border-2 bg-slate-50">
--- src/partials/roadmap/Header.tsx
+++ src/partials/roadmap/Header.tsx
@@ -6,15 +6,15 @@ export const Header = ({
     description: string;
 }) => {
     return (
-        <div className="flex w-full max-w-screen-lg flex-col px-8 pb-10 pt-6 md:flex-row md:items-end md:justify-between">
+        <div className="flex w-full max-w-screen-lg flex-col px-8 pb-8 pt-6 md:flex-row md:items-end md:justify-between">
             <div>
                 <span className="text-sm font-bold uppercase text-thinc-accent">
                     Roadmap
                 </span>
-                <h1 className="text-3xl font-semibold text-white">{title}</h1>
-                <p className="mt-1.5 text-slate-400 md:text-lg">
-                    {description}
-                </p>
+                <h1 className="mt-0 text-3xl font-semibold text-white">
+                    {title}
+                </h1>
+                <p className="-mt-1 text-slate-400 md:text-lg">{description}</p>
             </div>
             <button></button>
         </div>
