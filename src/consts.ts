b94d95e38a7cb20079b48ae15c6792f527e790ba
Ebonian <tanadon.santisan@gmail.com>
2023-10-09 17:34:32 +0700

    chore: update SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Thinc Knowledgee';
-export const SITE_DESCRIPTION = 'Thinc Knowledge';
+export const SITE_TITLE = 'Thinc. Knowledge';
+export const SITE_DESCRIPTION =
+    'แหล่งรวมความรู้ในการเริ่มต้นสำหรับการเริ่มต้นเป็น Developer ตั้งแต่ศูนย์จากบทความและ Roadmap ในการเริ่มต้นจากชมรม Thinc.';
